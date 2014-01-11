7ed7bb5c652a607763f740a5e738551a2efea86a
arpit_bhayani <arpit.b.bhayani@gmail.com>
2014-01-11 13:54:47 +0530

    WikiSE Hello World

---
+++ src/com/wikise/main/Main.java
@@ -0,0 +1,10 @@
+package com.wikise.main;
+
+/**
+ * Created by Arpit Bhayani on 11/1/14.
+ */
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Hello WikiSe");
+    }
+}
